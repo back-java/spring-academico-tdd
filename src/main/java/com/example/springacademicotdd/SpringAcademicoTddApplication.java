0e5039e14628f980101e86963f18915ebf4860e3
Robson Leal <robson_leal@outlook.com.br>
2022-08-26 15:39:41 -0300

    Começando projeto springboot

---
+++ src/main/java/com/example/springacademicotdd/SpringAcademicoTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.springacademicotdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademicoTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademicoTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springacademicotdd/SpringAcademicoTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springacademicotdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademicoTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
